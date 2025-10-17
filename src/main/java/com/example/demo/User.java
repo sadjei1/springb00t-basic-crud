741f89b15d9eb66302d7bcdb537ca8824ad8cbf0
sadjei1 <samuelkyeremeh19@yahoo.com>
2025-10-17 06:40:08 -0600

    additional changes

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -2,8 +2,15 @@ package com.example.demo;
 
 import com.fasterxml.jackson.annotation.JsonTypeId;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
@@ -13,4 +20,7 @@ public class User {
     private String firstName;
     private String lastName;
     private String email;
+
 }
+
+
--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -33,8 +35,48 @@ public class UserController {
 
     @PatchMapping("user/update/{id}")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userUpdate) {
+        try {
+            Optional<User> userOptional = userRepository.findById(id);
 
+            if (userOptional.isPresent()) {
+                User existingUser = userOptional.get();
+
+                // Update the fields you want to change
+                if (userUpdate.getFirstName() != null) {
+                    existingUser.setFirstName(userUpdate.getFirstName());
+                }
+                if (userUpdate.getEmail() != null) {
+                    existingUser.setEmail(userUpdate.getEmail());
+                }
+                // Add other fields as needed
+
+                User savedUser = userRepository.save(existingUser);
+                return ResponseEntity.ok(savedUser);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
+
+    @GetMapping("/user/all")
+    public ResponseEntity<List<User>> getUsers() {
+        List<User> allUsers = userRepository.findAll();
+
+        if (allUsers.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(allUsers);
+    }
+
+    @DeleteMapping("/user/delete/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+
 
 
 
